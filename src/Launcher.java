57913db003cfb5335322da8e7964e5b0f7491cd5
alexlyy <alexandre.ly@epita.fr>
2021-05-19 12:49:10 +0200

    test

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,14 +1,30 @@
 import java.util.Scanner;
 public class Launcher {
-    public static void main(String[] args)
-    {
-        Scanner myscanner = new Scanner(System.in);
-        System.out.println("Bienvenue");
-        String inputUser = myscanner.nextLine();
-        if (inputUser.equals("quit")) {
-            System.exit(0);
-         } else {
-            System.out.println("Unknown command");
+    public static void main(String[] args) {
+        System.out.println("Bienvenue,\nVeuillez saisir du texte :");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        while (!input.equals("quit")) {
+            if (input.equals("fibo")) {
+                System.out.println("Veuillez saisir la valeur de n :");
+                input = scan.nextLine();
+                System.out.println(fibonacci(Integer.parseInt(input)));
+            } else {
+                System.out.println("Unknown command");
+            }
+            input = scan.nextLine();
         }
+    }
+    private static int fibonacci(int n) {
+        int i = 0;
+        int j = 1;
+        int u, t;
+        for(u = 2; u < n; u++)
+        {
+            t = i+j;
+            i = j;
+            j = t;
+        }
+        return j;
     }
 }
